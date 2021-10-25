1d4f0063d0173041dd4b12ecd6595cfdfabcc79e
Wiktor <wiktor.tuminski@gmail.com>
2021-10-25 10:10:37 +0200

    Rozwiązane zadanie nr 4

--- lab_2/Exc_4/Exc_4.scala
+++ lab_2/Exc_4/Exc_4.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 // Zadanie 4. Zdefiniuj rekurencyjną funkcję ciąg(n: Int): Int, która zwróci n-ty wyrażony wzorem:
 // F(0) = 1
 // F(1) = 1
@@ -5,5 +6,14 @@
 // Rozwiąż to zadanie bez korzystania ze zmiennych oraz wykorzystaj rekurencję ogonową.
 
 object Main extends App {
-  def ciąg(n: Int): Int = {};
+  def ciąg(n: Int): Int = {
+    @tailrec
+    def helper(n: Int, a: Int = 0, b: Int = 1): Int = {
+      if (n == 0) b;
+      else helper(n - 1, b, a + b);
+    };
+    return helper(n);
+  };
+
+  print(ciąg(5))
 };
