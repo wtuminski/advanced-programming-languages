8f8a2aaa6059c28eaf0e83251d86b7a4ffa905ea
Wiktor <wiktor.tuminski@gmail.com>
2021-11-07 15:14:50 +0100

    Praca na zajęciach

---
+++ lab_3/Exc_1/Exc_1.scala
@@ -0,0 +1,26 @@
+// Zadanie 1. Zdefiniuj funkcję
+// def sum(l: List[Option[Int]]): Option[Int] = /* ... */
+// "sumującą”" listę opcjonalnych wartości całkowitych. Wynik powinien mieć wartość None wtedy i tylko wtedy, gdy wszystkie elementy listy l będą miały wartość None. W definicji skorzystaj z rekurencji ogonowej i "dopasowania wzorca".
+
+import scala.annotation.tailrec
+object Main extends App {
+  def sum(l: List[Option[Int]]): Option[Int] = {
+    @tailrec
+    def helper(
+        l: List[Option[Int]],
+        acc: Int = 0
+    ): Option[Int] = {
+      l match {
+        case Nil                       => Some(acc)
+        case None :: Nil if (acc == 0) => None
+        case None :: lT                => helper(lT, acc)
+        case Some(n) :: lT             => helper(lT, acc + n)
+      };
+    };
+    return helper(l);
+  };
+
+  println(sum(List(Some(1), Some(2), Some(3), Some(4), Some(5))));
+  println(sum(List(Some(1), Some(2), None)));
+  println(sum(List(None, None)));
+};
---
+++ lab_3/Exc_2/Exc_2.scala
@@ -0,0 +1,20 @@
+// Zadanie 2. Zdefiniuj następujące generyczne operacje na funkcjach
+
+//     "złożenie"
+//     def compose[A,B,C](f: A => B)(g: B => C): A => C
+//     "iloczyn"
+//     def prod[A,B,C,D](f: A => C, g: B => D): (A, B) => (C, D)
+//     "podniesienie operatora"
+//     def lift[A,T](op: (T,T) => T)(f: A => T, g: A => T): A => T
+
+object Main extends App {
+//   def compose[A, B, C](f: A => B)(g: B => C): A => C = {}
+
+  def prod[A, B, C, D](f: A => C, g: B => D): (A, B) => (C, D) = {
+    (a: A, b: B) => (f(a), g(b))
+  }
+//   def lift[A, T](op: (T, T) => T)(f: A => T, g: A => T): A => T = {
+//       (a:A) =>
+//   }
+
+}
