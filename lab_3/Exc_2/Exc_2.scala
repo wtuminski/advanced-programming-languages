89ab648ee0250bc2e39d8e8c7308a575546db6b4
Wiktor <wiktor.tuminski@gmail.com>
2021-11-07 15:39:39 +0100

    Zadanie nr 2 z laboratoriów nr 3

--- lab_3/Exc_2/Exc_2.scala
+++ lab_3/Exc_2/Exc_2.scala
@@ -8,13 +8,15 @@
 //     def lift[A,T](op: (T,T) => T)(f: A => T, g: A => T): A => T
 
 object Main extends App {
-//   def compose[A, B, C](f: A => B)(g: B => C): A => C = {}
+  def compose[A, B, C](f: A => B)(g: B => C): A => C = { (a: A) =>
+    (g(f(a)))
+  }
 
   def prod[A, B, C, D](f: A => C, g: B => D): (A, B) => (C, D) = {
     (a: A, b: B) => (f(a), g(b))
   }
-//   def lift[A, T](op: (T, T) => T)(f: A => T, g: A => T): A => T = {
-//       (a:A) =>
-//   }
 
+  def lift[A, T](op: (T, T) => T)(f: A => T, g: A => T): A => T = { (a: A) =>
+    op(f(a), g(a))
+  }
 }
