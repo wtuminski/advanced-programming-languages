9b4a3f4890349f72cd0e6a735ed7728760bb2fe5
Wiktor <wiktor.tuminski@gmail.com>
2021-11-08 20:12:43 +0100

    Zadanie nr 4 z laboratoriów nr 3

---
+++ lab_3/Exc_4.scala
@@ -0,0 +1,31 @@
+// Zadanie 4. Napisz generyczną funkcję
+// def divide[A](list: List[A]): (List[A], List[A]) = /* ... */
+// która podzieli listę list na dwie części. W pierwszej będą się znajdywać elementy na parzystych indeksach w drugiej elementy na nieparzystych. W definicji wykorzystaj rekurencję ogonową.
+// Przykład:
+// divide(List(1, 3, 5, 6, 7)) == (List(1, 5, 7), List(3, 6))
+
+package Exc_4
+
+import scala.annotation.tailrec
+
+object Main extends App {
+  def divide[A](list: List[A]): (List[A], List[A]) = {
+    @tailrec
+    def helper(
+        list: List[A],
+        evenList: List[A] = Nil,
+        oddList: List[A] = Nil,
+        index: Int = 0
+    ): (List[A], List[A]) = {
+      list match {
+        case Nil => (evenList, oddList)
+        case listH :: listT if (index % 2 == 0) =>
+          helper(listT, evenList :+ listH, oddList, index + 1)
+        case listH :: listT if (index % 2 != 0) =>
+          helper(listT, evenList, oddList :+ listH, index + 1)
+      };
+    };
+    return helper(list);
+  };
+  println(divide(List(1, 3, 5, 6, 7)));
+};
